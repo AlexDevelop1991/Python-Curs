68edcdaccd375bc7df72ea7ba3835d661ec4a635
AlexDevelop1991 <alexdevelop1991@gmail.com>
2023-04-09 21:55:01 +0300

    task3

--- Tasks/Task3.py
+++ Tasks/Task3.py
@@ -2,15 +2,27 @@
 my_dict = {}
 # Трижды попросите пользователя сначала ввести название ключа,потом ввести значение для этого ключа
 # Всего должно быть 6 запросов на ввод текста.
+# Во время получения данных от пользователя добавляйте в словарь ключи с значениями согласно тому что ввел пользователь.
 key_dict = input('Ведите первый предмет: ')
+value_dict = int(input(key_dict +'__' 'Введите оценку: '))
+
+my_dict[key_dict] = value_dict
+
 key2_dict = input('Введите второй предмет: ')
+value2_dict = int(input(key2_dict +'__' 'Введите оценку: '))
+
+my_dict[key2_dict] = value2_dict
+
 key3_dict = input('Введите третий предмет: ')
-total_key = key_dict, key2_dict, key3_dict
-my_dict.copy(total_key)
-value_dict = input(key_dict + 'Введите оценку: ')
-value2_dict = input(key2_dict + 'Введите оценку: ')
-value3_dict = input(key3_dict + 'Введите оценку: ')
-total_value = value_dict, value2_dict, value3_dict
-my_dict.copy(total_value)
+value3_dict = int(input(key3_dict +'__' 'Введите оценку: '))
+
+my_dict[key3_dict] = value3_dict
+
+# Вывести результат словаря в терминал
+print(my_dict)
+
+# Добавить самому в словарь ключ/значения.
+my_dict['Teacher'] = 'Mary'
+my_dict['Age'] = 30
 
 print(my_dict)
